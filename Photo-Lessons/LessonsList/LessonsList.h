efd3a73af9b6ba707173217de5ec826bc8ddfefb
TheusMartins <ml_martins@yahoo.com>
2023-01-14 20:11:08 -0300

    Created LessonsList framework

---
+++ Photo-Lessons/LessonsList/LessonsList.h
@@ -0,0 +1,18 @@
+//
+//  LessonsList.h
+//  LessonsList
+//
+//  Created by Matheus Martins on 14/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LessonsList.
+FOUNDATION_EXPORT double LessonsListVersionNumber;
+
+//! Project version string for LessonsList.
+FOUNDATION_EXPORT const unsigned char LessonsListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LessonsList/PublicHeader.h>
+
+
--- Photo-Lessons/Photo-Lessons/ContentView.swift
+++ Photo-Lessons/Photo-Lessons/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Network_Layer
 
 struct ContentView: View {
     var body: some View {
