a75c0970dc33212abafa771e6d9f8b0776dce786
TheusMartins <ml_martins@yahoo.com>
2023-01-14 08:23:27 -0300

    Creating base project

---
+++ Photo-Lessons/Photo-Lessons/Photo_LessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_LessonsApp.swift
+//  Photo-Lessons
+//
+//  Created by Matheus Martins on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Photo_LessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
